f60f7d8c2258270f1f004e4b915256522d8dfc5c
Alex Mizak <alexmmizak@gmail.com>
2022-03-22 20:55:02 -0400

    Personal Budget Tracker Alex Mizak sumbission

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    weeklyExpenses += parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    monthlyExpenses += parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    annualExpenses += parseFloat(window.prompt(annualExpenseQuestions[i]))
+}
